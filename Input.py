bc928bbcb7bf72cd9c99d4cbb72db19ef5769b16
HalfRobot <74208454+HalfRobotic@users.noreply.github.com>
2022-09-25 16:51:51 +0200

    Ejercicio simple

---
+++ Input.py
@@ -0,0 +1,11 @@
+num1 = int(input("Untrodueix un numero siusplau: "))
+num2 = int(input("Introdueix un segon número siusplau: "))
+suma = num1+num2
+resta = num1-num2
+multiplicacio = num1*num2
+divisio = num1/num2
+
+print("La suma de ", num1 , " i dona",num2, "un resultat de: ", suma )
+print("La resta de ", num1 , " i dona ",num2 ," un resultat de: ", resta )
+print("La multiplicació de ", num1 , " i dona ",num2 ," un resultat de: ", multiplicacio )
+print("La divisió de ", num1 , " i dona ",num2 ," un resultat de: ", divisio )
